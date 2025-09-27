e6c5baa287cd745baa36dc6214aaf4c2cac6e8e3
DVidal1205 <dylanvidal1204@gmail.com>
2025-09-27 04:07:26 -0400

    gitignore

--- jetbot-api/.ipynb_checkpoints/api-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from jetbot import Robot
-import time
-
-robot.left_motor.value = -0.5
-robot.right_motor.value = -0.5
-time.sleep(1)
-robot.stop()
--- jetbot-api/.ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,3 +0,0 @@
-# Main entry point for the JetBot API
-# Controls the WebSocket Server for Images and Control Data to the Web App
-# Controls the API Controller for operating the JetBot to be used by ADK.
